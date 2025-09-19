9744e7adf27065ee1e9130a02d0a2d5f77cb9240
aftb777 <aftaabmulla228@gmail.com>
2025-09-19 16:53:05 +0530

    Place Model

---
+++ CityTour/Models/PlaceModel.swift
@@ -0,0 +1,16 @@
+//
+//  PlaceModel.swift
+//  CityTour
+//
+//  Created by Aftaab Mulla on 19/09/25.
+//
+
+import Foundation
+ 
+struct PlaceModel{
+    let id : String
+    let name : String
+    let photoUrl : URL
+    let rating : Double
+    let address : String
+}
